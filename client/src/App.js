3df5e8b722028aadc663f2ebe55479148b657419
FMCalisto <FMCalisto@users.noreply.github.com>
2018-02-10 01:20:53 +0000

    [ADD] Routing & UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+import ButtonAdd from './components/ButtonAdd';
 
 const App = () => (
   <div>
-    <h1>R3LM</h1>
+    <Navbar />
+    <div className="container">
+      <Main />
+    </div>
+    <div className="fixed-action-btn">
+      <ButtonAdd />
+    </div>
   </div>
 )
 
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Lists from './Lists.js';
+import About from './About.js';
 
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Lists} />
+      <Route exact path='/about' component={About} />
+    </Switch>
+  </main>
+)
 
+export default Main;
