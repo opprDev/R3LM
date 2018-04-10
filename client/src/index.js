584a2bdf33e1f531b350d2edc7dd9bb1983c5e32
FMCalisto <FMCalisto@users.noreply.github.com>
2018-04-10 22:01:18 +0100

    [ADD] Redux & Stores & Providers

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+
+import rootReducer from './reducers';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { loadState } from './store/localStorage';
+
+const persistedState = loadState();
+
+const store = createStore(
+  rootReducer,
+  persistedState
+);
+
+console.log(store.getState());
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
